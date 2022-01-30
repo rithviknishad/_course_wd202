4e647c92fea6613a8bd2144589afc5b34b36b2f7
rithviknishad <rithvikn2001@gmail.com>
2022-01-30 19:15:41 +0530

    implements necessary views including bounty

--- GDC-Level-4-Milestone-master/tasks/views.py
+++ GDC-Level-4-Milestone-master/tasks/views.py
@@ -1,1 +1,58 @@
 # Add all your views here
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+tasks = []
+completed_tasks = []
+
+
+__HTTP_RESPONSE_REDIRECT_TO_TASKS = HttpResponseRedirect("/tasks")
+
+
+def tasks_view(request):
+    return render(
+        request,
+        "tasks.html",
+        {
+            "tasks": tasks,
+        },
+    )
+
+
+def completed_tasks_view(request):
+    return render(
+        request,
+        "completed_tasks.html",
+        {
+            "tasks": completed_tasks,
+        },
+    )
+
+
+def all_tasks_view(request):
+    return render(
+        request,
+        "all_tasks.html",
+        {
+            "tasks": [
+                *tasks,
+                *completed_tasks,
+            ],
+        },
+    )
+
+
+def add_task_view(request):
+    tasks.append(request.GET.get("task"))
+    return __HTTP_RESPONSE_REDIRECT_TO_TASKS
+
+
+def complete_task_view(request, index):
+    completed_tasks.append(tasks.pop(index - 1))
+    return __HTTP_RESPONSE_REDIRECT_TO_TASKS
+
+
+def delete_task_view(request, index):
+    del tasks[index - 1]
+    return __HTTP_RESPONSE_REDIRECT_TO_TASKS
