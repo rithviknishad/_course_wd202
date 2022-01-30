0fc4ea8e2af5eda9b06e7451371df8e6dce62fe6
rithviknishad <rithvikn2001@gmail.com>
2022-01-30 21:11:11 +0530

    fix missing trailing slash

--- GDC-Level-4-Milestone-master/task_manager/urls.py
+++ GDC-Level-4-Milestone-master/task_manager/urls.py
@@ -6,9 +6,9 @@ import tasks.views as views
 urlpatterns = [
     # path("admin/", admin.site.urls),
     path("tasks/", views.tasks_view),
-    path("completed-tasks/", views.completed_tasks_view),
+    path("completed_tasks/", views.completed_tasks_view),
     path("all-tasks/", views.all_tasks_view),
     path("add-task/", views.add_task_view),
-    path("complete-task/<int:index>", views.complete_task_view),
-    path("delete-task/<int:index>", views.delete_task_view),
+    path("complete_task/<int:index>/", views.complete_task_view),
+    path("delete-task/<int:index>/", views.delete_task_view),
 ]
