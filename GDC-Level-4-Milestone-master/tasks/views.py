9baacf2e009a01ae43a3690d6ff3401f0733a6c5
Rithvik Nishad <rithvikn2001@gmail.com>
2022-02-02 11:04:14 +0530

    Level 4 milestone (#1)
    * implements necessary views including bounty
    
    * include all necessary paths in `urls.py`
    
    * fix(`urls.py`): missing `complete_task_view`
    
    * implements `tasks.html`
    
    * implements `completed_tasks.html`
    
    * completes milestone
    
    * fix missing trailing slash

--- GDC-Level-4-Milestone-master/tasks/views.py
+++ GDC-Level-4-Milestone-master/tasks/views.py
@@ -1,1 +1,61 @@
 # Add all your views here
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+tasks = []
+completed_tasks = []
+
+
+__HTTP_RESPONSE_REDIRECT_TO_TASKS = HttpResponseRedirect("/tasks")
+
+
+def tasks_view(request):
+    return render(
+        request,
+        "tasks.html",
+        {
+            "tasks": tasks,
+            "has_pending": bool(tasks),
+        },
+    )
+
+
+def completed_tasks_view(request):
+    return render(
+        request,
+        "completed_tasks.html",
+        {
+            "tasks": completed_tasks,
+        },
+    )
+
+
+def all_tasks_view(request):
+    return render(
+        request,
+        "all_tasks.html",
+        {
+            "tasks": [
+                *tasks,
+                *completed_tasks,
+            ],
+        },
+    )
+
+
+def add_task_view(request):
+    task = request.GET.get("task")
+    if task:
+        tasks.append(task)
+    return __HTTP_RESPONSE_REDIRECT_TO_TASKS
+
+
+def complete_task_view(request, index):
+    completed_tasks.append(tasks.pop(index - 1))
+    return __HTTP_RESPONSE_REDIRECT_TO_TASKS
+
+
+def delete_task_view(request, index):
+    del tasks[index - 1]
+    return __HTTP_RESPONSE_REDIRECT_TO_TASKS
