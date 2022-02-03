3744b9e5cb66a4ab44a2446bf630691eadc3e311
rithviknishad <rihtvikn2001@gmail.com>
2022-02-03 15:42:05 +0530

    complete level 5 milestone including bounty

--- GDC-Level-5-Milestone-master/task_manager/urls.py
+++ GDC-Level-5-Milestone-master/task_manager/urls.py
@@ -1,6 +1,15 @@
+from django import views
 from django.contrib import admin
 from django.urls import path
 
+from tasks.views import *
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tasks/", tasks_view),
+    path("completed_tasks/", completed_tasks_view),
+    path("all_tasks/", all_tasks_view),
+    path("add-task/", add_task_view),
+    path("complete_task/<int:id>/", complete_task_view),
+    path("delete-task/<int:id>/", delete_task_view),
 ]
