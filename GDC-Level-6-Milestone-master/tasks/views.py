5c3b359ab71e2f646f187799144c2f228088a1d7
rithviknishad <rithvikn2001@gmail.com>
2022-02-09 13:39:18 +0530

    updates views.py

--- GDC-Level-6-Milestone-master/tasks/views.py
+++ GDC-Level-6-Milestone-master/tasks/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from re import template
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.forms import ModelForm, ValidationError
@@ -11,23 +13,46 @@ from tasks.models import Task
 
 class AuthorizedTaskManager(LoginRequiredMixin):
     def get_queryset(self):
-        return Task.objects.filter(
-            deleted=False,
-            user=self.request.user,
-        )
+        return Task.objects.filter(deleted=False, user=self.request.user)
 
 
-class UserLoginView(LoginView):
-    template_name = "user_login.html"
+class UserAuthFormViewMixin:
+    template_name = "user_auth_form.html"
+    auth_action = "Auth"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.auth_action
+        context["auth_action"] = self.auth_action
+        return context
 
 
-class UserLoginView(LoginView):
-    template_name = "user_login.html"
+class FormStyleMixin(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""  # Removes trailing colon present in form field labels.
+        # Apply style to all form fields.
+        style = "bg-[#F1F5F9] appearance-none border-2 border-[#F1F5F9] rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = style
 
 
-class UserCreateView(CreateView):
-    form_class = UserCreationForm
-    template_name = "user_create.html"  # TODO: implement this template.
+class LoginForm(FormStyleMixin, AuthenticationForm):
+    pass
+
+
+class SignUpForm(FormStyleMixin, UserCreationForm):
+    pass
+
+
+class UserLoginView(UserAuthFormViewMixin, LoginView):
+    form_class = LoginForm
+    auth_action = "Login"
+
+
+class UserCreateView(UserAuthFormViewMixin, CreateView):
+    form_class = SignUpForm
+    auth_action = "Sign Up"
     success_url = "/user/login"
 
 
@@ -49,7 +74,7 @@ class CompletedTaskView(GenericTaskView):
         return super().get_queryset().filter(completed=True)
 
 
-class TaskCreateForm(ModelForm):
+class TaskForm(ModelForm):
     def clean_title(self):
         title = self.cleaned_data["title"]
         if len(title) < 4:
@@ -68,7 +93,7 @@ class TaskCreateForm(ModelForm):
 
 # TODO: test access of this view without authenticating.
 class TaskCreateView(AuthorizedTaskManager, CreateView):
-    form_class = TaskCreateForm
+    form_class = TaskForm
     template_name = "task_create.html"
     success_url = "/tasks"
 
@@ -81,7 +106,7 @@ class TaskCreateView(AuthorizedTaskManager, CreateView):
 
 class TaskUpdateView(AuthorizedTaskManager, UpdateView):
     model = Task
-    form_class = TaskCreateForm
+    form_class = TaskForm
     template_name = "task_update.html"
     success_url = "/tasks"
 
