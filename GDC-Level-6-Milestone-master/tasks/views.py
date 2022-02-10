90b034ee3a46139732bc569c89e639702b190a67
rithviknishad <rithvikn2001@gmail.com>
2022-02-10 16:06:59 +0530

    implements task views

--- GDC-Level-6-Milestone-master/tasks/views.py
+++ GDC-Level-6-Milestone-master/tasks/views.py
@@ -56,19 +56,43 @@ class UserSignupView(__UserAuthenticationViewMixin, CreateView):
 
 
 class GenericTaskView(__AuthorizedTaskManager, ListView):
+    model = Task
     template_name = "tasks.html"
+    context_object_name = "tasks"
+
+    def get_tasks_status_description(self):
+        all = super().get_queryset()
+        completed = all.filter(completed=True)
+        return f"{len(completed)} of {len(all)} completed"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_header"] = f"Hi {self.request.user}"
+        context["tasks_status_description"] = self.get_tasks_status_description()
+        context["page_tabs"] = {
+            tab.view_name: (tab.path if self.view_name != tab.view_name else None)
+            for tab in [AllTaskView, PendingTaskView, CompletedTaskView]
+        }
+        return context
 
 
 class AllTaskView(GenericTaskView):
-    pass
+    view_name = "All"
+    path = "/tasks"
 
 
 class PendingTaskView(GenericTaskView):
+    view_name = "Pending"
+    path = "/tasks/pending"
+
     def get_queryset(self):
         return super().get_queryset().filter(completed=False)
 
 
 class CompletedTaskView(GenericTaskView):
+    view_name = "Completed"
+    path = "/tasks/completed"
+
     def get_queryset(self):
         return super().get_queryset().filter(completed=True)
 
