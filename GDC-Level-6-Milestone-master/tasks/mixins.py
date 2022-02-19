5076470838fc2e979e2b0880f5edc4bd82783866
rithviknishad <rithvikn2001@gmail.com>
2022-02-19 18:42:39 +0530

    chore

--- GDC-Level-6-Milestone-master/tasks/mixins.py
+++ GDC-Level-6-Milestone-master/tasks/mixins.py
@@ -36,7 +36,7 @@ class AuthFormMixin(FormStyleMixin):
 
 class AuthViewMixin:
     template_name = "user_auth_form.html"
-    auth_action = "Auth"
+    auth_action = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
