ca13656efcc0398a1c8965a9d4448e6514511c60
rithviknishad <rithvikn2001@gmail.com>
2022-02-21 19:48:05 +0530

    fix missing authorisation for task status history api

--- GDC-Level-6-Milestone-master/tasks/apiviews.py
+++ GDC-Level-6-Milestone-master/tasks/apiviews.py
@@ -86,3 +86,6 @@ class TaskStatusChangesViewSet(ReadOnlyModelViewSet):
     permission_classes = (IsAuthenticated,)
     filter_backends = (DjangoFilterBackend,)
     filterset_class = TaskStatusChangesFilter
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user, deleted=False)
