0cfc44c79c510fae82d1bb2dfc42116445bb2de5
rithviknishad <rithvikn2001@gmail.com>
2022-02-24 11:53:15 +0530

    registers new models in admin sites

--- GDC-Level-6-Milestone-master/tasks/admin.py
+++ GDC-Level-6-Milestone-master/tasks/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from tasks.models import Task
+from tasks.models import Task, TaskStatusChangeLog, UserReportConfiguration
 
 admin.sites.site.register(Task)
+admin.sites.site.register(TaskStatusChangeLog)
+admin.sites.site.register(UserReportConfiguration)
