3652241dab5f90fe38a2850817a094d7f6b88807
rithviknishad <rithvikn2001@gmail.com>
2022-02-27 21:44:08 +0530

    adds test

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+
+
+class QuestionModelTests(TestCase):
+    def test_always_fail(self):
+        """
+        This test will always fail ( for now )
+        """
+        self.assertIs(True, False)
